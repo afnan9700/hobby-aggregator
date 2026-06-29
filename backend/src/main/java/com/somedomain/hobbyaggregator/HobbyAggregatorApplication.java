a885b5d0d4e5ab8083c40ec2140042eae0dcad42
afnan9700 <afnanshaik970@gmail.com>
2026-06-29 12:59:28 +0530

    project init

---
+++ backend/src/main/java/com/somedomain/hobbyaggregator/HobbyAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.somedomain.hobbyaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyAggregatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/somedomain/hobbyaggregator/HobbyaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somedomain.hobbyaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
